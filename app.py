42b33b769785549f2967c5fb9c5fe5b4a49bb047
Abhishek Singh <abhigkv3@gmail.com>
2026-05-30 11:59:27 +0530

    Replaced ChromaDB with FAISS

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
@@ -59,7 +57,7 @@ def load_rag_system(api_key):
         embeddings = HuggingFaceEmbeddings(
             model_name="all-MiniLM-L6-v2"
         )
-        vectordb = Chroma.from_documents(
+        vectordb = FAISS.from_documents(
             documents=chunks,
             embedding=embeddings
         )
